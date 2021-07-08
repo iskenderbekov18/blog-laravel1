6f3b1d82795df4e170dcb9c68374f2484e421f5b
iskenderbekov18 <bekzhan.bolotbekov@gmail.com>
2021-07-08 21:59:43 +0600

    /add blog 1

---
+++ blog/resources/views/layout/scripts.blade.php
@@ -0,0 +1,4 @@
+<!-- JAVASCRIPTS -->
+<script src="{{ asset('layout/scripts/jquery.min.js') }}"></script>
+<script src="{{ asset('layout/scripts/jquery.backtotop.js') }}"></script>
+<script src="{{ asset('layout/scripts/jquery.mobilemenu.js') }}"></script>
